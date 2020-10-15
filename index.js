a70a8190ccfd3b6cdf91ab9e5ba1b6f084c82efa
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-15 13:30:20 +0200

    add event bus

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(morgan('dev'));
 const posts = {};
+
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/events', async (req, res) => {
+app.post('/events', (req, res) => {
     const { type, data } = req.body;
-
-    console.log('req.body ===>', req.body);
 
     if (type === 'PostCreated') {
         const { id, title } = data;
+
         posts[id] = { id, title, comments: [] };
     }
+
     if (type === 'CommentCreated') {
         const { id, content, postId } = data;
 
@@ -27,7 +28,11 @@ app.post('/events', async (req, res) => {
         post.comments.push({ id, content });
     }
 
+    console.log(posts);
+
     res.send({});
 });
 
-app.listen(4002, () => console.log("Server running on port 4002"));
+app.listen(4002, () => {
+    console.log('Listening on 4002');
+});
