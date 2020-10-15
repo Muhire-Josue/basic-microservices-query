b99e00f4556ad5e6092ebcdd41abd93e01130f1d
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-15 12:17:09 +0200

    add event bus

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-
+const posts = {};
 app.get('/posts', (req, res) => {
 
 });
 
 app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
 
+    if (type === 'PostCreated') {
+        const { id, content } = data;
+        posts[id] = { id, content, comments: [] };
+    }
 });
 
 app.listen(4002, () => console.log("Server running on port 4002"));
